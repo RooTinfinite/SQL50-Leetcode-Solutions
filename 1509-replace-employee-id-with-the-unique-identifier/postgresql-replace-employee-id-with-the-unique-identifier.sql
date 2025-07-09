d33b720d6a601e75ca246f3551c8c0b9de69f432
mary6559 <maria.adamczyk22@gmail.com>
2025-07-09 22:25:34 +0200

    Add PostgreSQL solutions for three SQL problems
    Implemented PostgreSQL queries for problems 1509 (replace employee id with unique identifier), 584 (find customer referee), and 595 (big countries). Each file now contains the appropriate SQL SELECT statements to solve the respective problems.

--- 1509-replace-employee-id-with-the-unique-identifier/postgresql-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/postgresql-replace-employee-id-with-the-unique-identifier.sql
@@ -1,1 +1,13 @@
 -- TODO: PostgreSQL solution
+
+---Version with LEFT JOIN ... ON
+SELECT EU.unique_id, E.name
+FROM Employees AS E
+LEFT JOIN EmployeeUNI AS EU 
+    ON E.id = EU.id;
+
+---Version with LEFT JOIN ... USING
+SELECT unique_id, name
+FROM Employees
+LEFT JOIN EmployeeUNI 
+    USING (id);
