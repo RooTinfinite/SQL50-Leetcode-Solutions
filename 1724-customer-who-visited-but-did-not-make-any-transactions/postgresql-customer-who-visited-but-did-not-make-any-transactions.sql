5781c87c8f4c921cfe80ad28c84d761bd08e67a1
mary6559 <maria.adamczyk22@gmail.com>
2025-07-10 14:55:49 +0200

    Add PostgreSQL solutions for two SQL problems
    Implemented PostgreSQL queries for 'Customer Who Visited but Did Not Make Any Transactions' using COUNT() and ARRAY_AGG(), and for 'Rising Temperature' using CROSS JOIN, JOIN ... ON, and JOIN ... USING with INTERVAL. These additions provide working solutions for both problems.

--- 1724-customer-who-visited-but-did-not-make-any-transactions/postgresql-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/postgresql-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,1 +1,18 @@
 -- TODO: PostgreSQL solution
+
+--VERSION WITH COUNT()
+SELECT v.customer_id, COUNT(visit_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+    USING (visit_id)
+WHERE t.transaction_id IS NULL
+GROUP BY customer_id;
+
+--VERSION WITH ARRAY_AGG() 
+SELECT v.customer_id, 
+       ARRAY_LENGTH(array_agg(v.visit_id), 1) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+    ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY customer_id;
--- 197-rising-temperature/postgresql-rising-temperature.sql
+++ 197-rising-temperature/postgresql-rising-temperature.sql
@@ -1,1 +1,23 @@
 -- TODO: PostgreSQL solution
+
+--Version with CROSS JOIN:
+SELECT today.id
+FROM Weather AS yesterday
+CROSS JOIN Weather AS today
+WHERE today.recordDate - yesterday.recordDate = 1
+    AND today.temperature > yesterday.temperature;
+
+--Version with JOIN ... ON:
+SELECT today.id
+FROM Weather today
+JOIN Weather yesterday
+    ON today.recorddate = yesterday.recorddate + 1
+WHERE today.temperature > yesterday.temperature;
+
+--Alternative version with JOIN ... USING with INTERVAL::
+SELECT today.id 
+FROM Weather yesterday
+JOIN Weather today
+    ON today.recorddate = yesterday.recorddate + INTERVAL '1 day'
+WHERE today.temperature > yesterday.temperature;
+
