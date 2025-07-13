afa1f2185d31317095d3bd19108421d992268b18
mary6559 <maria.adamczyk22@gmail.com>
2025-07-13 14:06:35 +0200

    feat: contest attendance percentage
    Implements a query to calculate the percentage of users who attended each contest, rounding to two decimal places and ordering by percentage and contest ID.

--- 1773-percentage-of-users-attended-a-contest/postgresql-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/postgresql-percentage-of-users-attended-a-contest.sql
@@ -1,1 +1,10 @@
 -- TODO: PostgreSQL solution
+
+SELECT contest_id,
+    ROUND( 100.0 *
+        COUNT( DISTINCT user_id )
+        / ( SELECT COUNT( user_id ) FROM Users )
+    , 2 ) AS percentage
+FROM Register 
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id;
