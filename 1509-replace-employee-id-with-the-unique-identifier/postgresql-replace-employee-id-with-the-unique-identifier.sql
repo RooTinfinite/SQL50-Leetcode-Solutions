6a5e265437673b3ea7217c80997b2c293bde47aa
Maria <maria.adamczyk22@gmail.com>
2025-08-12 17:17:06 +0200

    Update postgresql-replace-employee-id-with-the-unique-identifier.sql

--- 1509-replace-employee-id-with-the-unique-identifier/postgresql-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/postgresql-replace-employee-id-with-the-unique-identifier.sql
@@ -1,10 +1,12 @@
 ---Version with LEFT JOIN ... ON
+
 SELECT EU.unique_id, E.name
 FROM Employees AS E
 LEFT JOIN EmployeeUNI AS EU 
     ON E.id = EU.id;
 
 ---Version with LEFT JOIN ... USING
+
 SELECT unique_id, name
 FROM Employees
 LEFT JOIN EmployeeUNI 
