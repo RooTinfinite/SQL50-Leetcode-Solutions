5ac2e19e0bdabc9bdb453332ee70cacb21e1df22
Maria <maria.adamczyk22@gmail.com>
2025-08-12 17:17:30 +0200

    Update postgresql-customer-who-visited-but-did-not-make-any-transactions.sql

--- 1724-customer-who-visited-but-did-not-make-any-transactions/postgresql-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/postgresql-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,6 +1,5 @@
--- TODO: PostgreSQL solution
-
 --VERSION WITH COUNT()
+
 SELECT v.customer_id, COUNT(visit_id) AS count_no_trans
 FROM Visits v
 LEFT JOIN Transactions t
@@ -9,6 +8,7 @@ WHERE t.transaction_id IS NULL
 GROUP BY customer_id;
 
 --VERSION WITH ARRAY_AGG() 
+
 SELECT v.customer_id, 
        ARRAY_LENGTH(array_agg(v.visit_id), 1) AS count_no_trans
 FROM Visits v
